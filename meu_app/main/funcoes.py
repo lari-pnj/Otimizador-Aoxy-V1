56d5653cd7abeff2f65b8826b8aaa78247295c25
Larissa valleriano <larissavaleriano36@gmail.com>
2026-09-18 16:23:28 -0300

    novas funções adicionadas

--- meu_app/main/funcoes.py
+++ meu_app/main/funcoes.py
@@ -1,24 +1,23 @@
 import os
 import subprocess
 import shutil
+import winreg
+import time
 
+# função 1
 def desativar_recursos_func():
      os.startfile(r"C:\Windows\System32\OptionalFeatures.exe")
      
-
-# Abre os programas instalados no pc
+# função 2
 def desinstalar_app_func():
      subprocess.run('appwiz.cpl', shell = True)
      
-
-# Verificar drives a serem atualizados
+# função 3
 def atualizar_drives_func():
      os.startfile("devmgmt.msc")
 
-
-# Função para limpar pastas com arquivos desnecessarios
-
-def limpar_arquivos_desnecessarios_func():
+# função 4
+def limpar_cache_windows_func():
      # Pastas a limpar
      pastas = [r"C:\Windows\Temp",# Pasta Temp do sistema
      os.environ.get('TEMP')# Pasta Temp do usuário (%Temp%)
@@ -41,12 +40,12 @@ def limpar_arquivos_desnecessarios_func():
                print(f"Pasta não encontrada: {pasta}")
 
 
-# Abre a janela de Opções de Energia
+# função 5
 def recursos_energia_func():
      subprocess.run('powercfg.cpl', shell = True)
 
   
-# Abrir Configurações Avançadas de Desempenho (Efeitos Visuais)
+# função 6
 def configuracoes_visuais_func():
       # Usa caminho absoluto para evitar dependência do PATH
       systemroot = os.environ.get('SystemRoot', r'C:\Windows')
@@ -58,35 +57,144 @@ def configuracoes_visuais_func():
            subprocess.run("SystemPropertiesPerformance.exe", shell=True)
 
 
-# DELETA A PASTA PREFETCH
-def limpar_prefetch_temp_func():
-     prefetch_path = r"C:\Windows\Prefetch"
+# função 7
+#def limpar_prefetch_temp_func():
+#     prefetch_path = r"C:\Windows\Prefetch"
 
      # Verifica se a pasta existe 
-     if os.path.exists(prefetch_path):
-          for file in os.listdir(prefetch_path):
-               file_path = os.path.join(prefetch_path, file)
-               try:
-                    if os.path.isfile(file_path):
-                         os.remove(file_path)  # Apaga arquivos
-                         print(f"Apagado: {file_path}")
-               except PermissionError:
-                    print(f"Sem permissão para apagar: {file_path}")
-               except Exception as e:
-                    print(f"Erro ao apagar {file_path}: {e}")
-          else:
-           print("Pasta Prefetch não encontrada.")
+#     if os.path.exists(prefetch_path):
+#          for file in os.listdir(prefetch_path):
+#               file_path = os.path.join(prefetch_path, file)
+#               try:
+#                    if os.path.isfile(file_path):
+#                         os.remove(file_path)  # Apaga arquivos
+#                         print(f"Apagado: {file_path}")
+#               except PermissionError:
+#                    print(f"Sem permissão para apagar: {file_path}")
+#                    time.sleep(2)
+#               except Exception as e:
+#                    print(f"Erro ao apagar {file_path}: {e}")
+#                    time.sleep(2)
+#          else:
+#           print("Pasta Prefetch não encontrada.")
 
 
-# Abre a pasta de inicialização do usuário
-
+# função 8
 def apps_inicializacao_func(): 
     subprocess.run("taskmgr", shell=True)
     
-# Abre o monitor de recursos para monitorar a temperatura
+# função 9
 def monitorar_temperatura_func():
      subprocess.run("resmon.exe", shell=True)
 
 
 
+################################################# FUNÇÕES A SEREM ENCREMENTADAS ##############################################
 
+# função 10
+def desativar_apps_seg_plan_func(): 
+     path = (
+           r'Software\Microsoft\Windows\CurrentVersion\BackgroundAcessAplications'
+      )
+     try:
+        key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, path)
+        winreg.SetValueEx(key, "GlobalUserDisabled", 0, winreg.REG_DWORD, 1)
+        winreg.CloseKey(key)
+        print('[Sucesso] Apps em segundo plano desativados.')
+        time.sleep(2)
+     except Exception as e:
+        print(f'[Erro] Apps segundo plano {e}')  
+        time.sleep(2) 
+
+
+# função 11 (revisar)
+#def desativar_relatorios_erro_func():
+ #    path = (
+ #         r'SOFTWARE\Microsoft\Windows\Windows Error Reporting'        
+ #    )
+ #    try:
+ #        key = winreg.Createkey(winreg.HKEY_LOCAL_MACHINE, path)
+ #        winreg.SetValueEx(key, "disable", 0, winreg.REG_DWORD, 1)
+ #        winreg.CloseKey(key)
+ #    except Exception as e:
+ #         print(f'[Erro Registro WER]: {e}')
+ #         time.sleep(2)
+
+ #   subprocess.run('net stop WerSvc', shell=True, capture_output=True)
+ #    subprocess.run(
+ #         'sc config WerSvc start= disable', shell=True, capture_output=True
+ #    )     
+ #    print('[Sucesso] Relatórios de erro desativados.') 
+ #    time.sleep(2)   
+
+
+
+# FUNÇÕES QUE ALTERAM OS REGISTROS #############################################
+
+def alterar_registros(hive, path, nome_valor, valor, tipo=winreg.REG_DWORD):
+     try:
+        key = winreg.CreateKey(hive, path)
+        winreg.SetValueEx(key, nome_valor, 0, tipo, valor)
+        winreg.CloseKey(key)
+        print(f'[Sucesso] Chave alterada: {nome_valor}')
+     except Exception as e:
+          print(f'[Erro] Falha ao alterar {nome_valor}: {e}')
+
+
+# função 12
+# Desativar Cortana
+#     alterar_registros(
+#          winreg.HKEY_LOCAL_MACHINE,
+#          r'SOFTWARE\policies\microsoft\windows Search', 
+#          'AllowCortana', 0,)
+
+# função 13    
+# Otimizar gamebar
+#     alterar_registros(
+#          winreg.HKEY_CURRENT_USER, r'System\GameConfigStore', 'GameDVR_Enabled', 0 )
+
+# função 14     
+# Aumentar prioridade da cpu/gpu
+#     alterar_registros(
+#          winreg.HKEY_LOCAL_MACHINE,
+#          r'SYSTEM\CurrentControlSet\Control\PriorityControl',
+#          'Win32PrioritySeparation', 38,)
+
+
+# função 15
+def desativar_telemetria_func():
+     path = r'SOFTWARE\Policies\Microsoft\windows\DataCollection'
+     try:
+        key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, path)
+        winreg.SetValueEx(key, 'AllowTelemetry', 0, winreg.REG_DWORD, 0)
+        winreg.Closekey(key)
+     except Exception as e:
+          print('[Sucesso] Telemetria desativada.')
+          time.sleep(2)   
+
+
+# função 16
+def aumentar_foreground_func():
+     path = r'SYSTEM\CurrentControlSet\Control\PriorityControl'
+     try:
+        key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, path)
+        winreg.SetValueEx(
+             key, "Win32PrioritySeparation", 0, winreg.REG_DWORD, 38
+        ) 
+        winreg.CloseKey(key)
+        print("[Sucesso] Prioridades Foreground aumentada.")
+        time.sleep(2)
+     except Exception as e:
+          print(f"[Erro prioridade foreground]: {e}")   
+          time.sleep(2)
+
+
+# função 17
+# 9- Melhorar Conexão de ping (Dns jumper)
+#def melhorar_ping_func(ip_dns='1.1.1.1'):
+#cmd = ('powershell -Command "Get-NetAdapter | where-Object {$_.Status -eq \'up\'}'
+#     f" | Set-DnsClientServerAddress -ServerAddresses ('{ip_dns}')\"")
+#executar_comando(cmd)
+#print(f'[Rede] DNS alterado para {ip_dns}')
+
+#melhorar_ping_func('1.1.1.1')
--- meu_app/main/main.py
+++ meu_app/main/main.py
@@ -2,31 +2,82 @@ from funcoes import *
 import subprocess
 import os
 import time
+import shutil
+from itertools import zip_longest
+import ctypes
+import sys
 
 
+# Solicita a permissao de administrador
+
+#if not ctypes.windll.shell32.isUserAnAdmin():
+#   ctypes.windll.shell32.shellExecuteW(
+#      None, 'runas', sys.executable, ' '.join(sys.argv), None, 1
+#   )
+#   sys.exit()
+
+   
 def menu():
-    # Tela inicial
-    print("====="*10)
-    print("--------------  Otimizador AOXY  -----------------")
-    print("====="*10)
+    
+    titulo()
 
-    # LISTA DE FUNÇÕES
-    opcoes = ['[1] ➤  Desativar recursos','[2] ➤  Desinstalar apps','[3] ➤  Atualizar Drivers',
-              '[4] ➤  Limpar Arquivos Desnecessarios','[5] ➤  Recursos De Energia',
-              '[6] ➤  Configurações Visuais','[7] ➤  Limpar Prefetch',
-              '[8] ➤  Apps De Inicialização','[9] ➤  Monitorar Temperatura','[10] ➤  SAIR']
-    for i in opcoes:
-         print(i)
-    print('')  
+    # LISTA DE FUNÇÕES 1
+    opcoes = ['[1] ➤  Desativar recursos',
+             '[2] ➤  Desinstalar apps',
+             '[3] ➤  Atualizar Drivers',
+             '[4] ➤  Limpar Cache do Windows',
+             '[5] ➤  Recursos De Energia',
+             '[6] ➤  Configurações Visuais',
+             '[7] ➤  Limpar Prefetch',
+             '[8] ➤  Apps De Inicialização',
+             '[9] ➤  Monitorar Temperatura',
+             '[10] ➤  Desativar Apps de Segundo plano',
+             '[11] ➤  Otimizar gamebar',
+             '[12] ➤  Desativar Relatorios de erro',
+             '[13] ➤  Desativar Cortana', 
+             '[14] ➤  Aumentar prioridade da cpu/gpu', 
+             '[15] ➤  Desativar telemetria' , 
+             '[16] ➤  Aumentar Prioridade Foreground', 
+             '[17] ➤  Melhorar Conexão de ping',
+             '[18] ➤  EXIT']
+    
+    metade = len(opcoes)  // 2
+    coluna1 = opcoes[:metade]
+    coluna2 = opcoes[metade:]
 
+    largura_col1 = 40
+    largura_bloco = 75
+    largura_terminal = shutil.get_terminal_size().columns
+    margem_esquerda = max(0, (largura_terminal - largura_bloco) //2)
+    espacos = " " * margem_esquerda
+
+       
+    for item1, item2 in zip_longest(coluna1, coluna2, fillvalue=""):
+          print(f'{espacos}{item1:<{largura_col1}}{item2}')
+          print()  
+
+
+# titulo
+
+def titulo():
+   laranja = '\033[38;2;255;165;0m'
+   reset ='\033[0m'
+   titulo = "======== AOXY V1 ======="
+   print('')
+   print(f"{laranja}{titulo.center(shutil.get_terminal_size().columns)}{reset}") 
+   print('\n')
+   
 #Função principal para escolha de opções
 
 def main():
      while True:
-        os.system('cls' if os.name =='nt' else 'clear')
         
+        os.system('cls' if os.name =='nt' else 'clear')
+
         menu()
+
         escolha = input('Escolha Uma Opção: ')
+
         if escolha == '1':
             print('1- DESATIVAR RECURSOS')
             time.sleep(2)
@@ -43,9 +94,9 @@ def main():
            atualizar_drives_func()
            
         elif escolha == '4':   
-           print('4- LIMPAR ARQUIVOS DESNECESSARIOS')
+           print('4- LIMPAR CACHE DO WINDOWS')
            time.sleep(5)
-           limpar_arquivos_desnecessarios_func()
+           limpar_cache_windows_func()
            
         elif escolha == '5':   
            print('5- RECURSOS DE ENERGIA')
@@ -71,9 +122,61 @@ def main():
           print('MONITORAR TEMPERATURA')
           time.sleep(2)
           monitorar_temperatura_func()
-          
+
+        elif escolha == '10':
+          print('Desativar Apps de segundo plano') 
+          time.sleep(2)   
+          desativar_apps_seg_plan_func() 
+
+
+# Função que altera o registro
+        elif escolha == '11': 
+            print('Otimizar gamebar')
+            time.sleep(2)
+            alterar_registros(winreg.HKEY_CURRENT_USER, r'System\GameConfigStore', 'GameDVR_Enabled', 0 )
+
+        elif escolha == '12': 
+            print('Desativar Relatorios de erro')
+            time.sleep(2)
+            desativar_relatorios_erro_func()
+
+        elif escolha == '13': 
+            print('Desativar Cortana')
+            time.sleep(2)
+            alterar_registros(
+                      winreg.HKEY_LOCAL_MACHINE,
+                      r'SOFTWARE\policies\microsoft\windows Search', 
+                      'AllowCortana', 0,)
+
+        elif escolha == '14': 
+            print('Aumentar prioridade da cpu/gpu')
+            time.sleep(2)
+            alterar_registros(
+                     winreg.HKEY_LOCAL_MACHINE,
+                     r'SYSTEM\CurrentControlSet\Control\PriorityControl',
+                     'Win32PrioritySeparation',
+                     38,)
+   
+        elif escolha == '15': 
+            print('Desativar telemetria')
+            time.sleep(2)
+            desativar_telemetria_func()
+
+        elif escolha == '16': 
+            print('Aumentar Prioridade Foreground')
+            time.sleep(2)
+            aumentar_foreground_func()
+
+        elif escolha == '17': 
+            print('Melhorar Conexão de ping')
+            time.sleep(2)
+            melhorar_ping_func('1.1.1.1')  
+
+        elif escolha == '18':
+           print('saindo.....')  
+           break                                                                    
+                       
         else:
-          print('saindo.....')
           break
 
 if __name__ == '__main__':
